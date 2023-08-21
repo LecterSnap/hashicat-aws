867dc10e899768925889937812b4d30515900dd5
lecter <100750420+LecterSnap@users.noreply.github.com>
2023-08-21 20:39:48 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3_bucket" {
   source  = "app.terraform.io/lecter-aws/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "lecter_20230821_terraform_bucket"
+  bucket_prefix = "lecter-20230821-terraform-bucket"
 }
